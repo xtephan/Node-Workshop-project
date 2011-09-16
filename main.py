f2805b93efb89998e42de39e3a94b783702cf279
Stefan Fodor <xtephan@gmail.com>
2011-09-16 08:33:35 +0200

    echo server with bug

--- main.py
+++ main.py
@@ -6,11 +6,17 @@ Created on 15 Sep 2011
 
 from server.server  import Server 
 
+IP   = "127.0.0.1"
+PORT = 1666
+
 if __name__ == '__main__':
     
     srv = Server()
     
     srv.hello()
+    
+    #echo server. will replay everything you say
+    srv.echo_server(IP, PORT)
     
     
     
--- server/server.py
+++ server/server.py
@@ -4,6 +4,7 @@ Created on 15 Sep 2011
 @author: xtephan
 '''
 
+from server.echoserver import EchoServer
 
 class Server():
     '''
@@ -16,3 +17,10 @@ class Server():
         '''
         print("Hello world! I am the server class")
         
+        
+    def echo_server(self,ip,port):
+        
+        es = EchoServer(ip,port)
+        
+        es.run()
+        #FIXME: put no replay
