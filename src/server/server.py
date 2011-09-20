835fd8b17f320c96dad4494fba3b12c5afb79c11
Stefan Fodor <xtephan@gmail.com>
2011-09-20 11:11:24 +0200

    First Commit. Echo server working

---
+++ src/server/server.py
@@ -0,0 +1,28 @@
+'''
+Created on 15 Sep 2011
+
+@author: xtephan
+'''
+
+from server.echoserver import EchoServer
+
+class Server():
+    '''
+    classdocs
+    '''
+
+    def hello(self):
+        '''
+        Return hello
+        '''
+        print("Hello world! I am the server class")
+        
+        
+    def echo_server(self,ip,port):
+        
+        es = EchoServer(ip,port)
+        
+        es.set_replay(False)
+        
+        es.run()
+        
