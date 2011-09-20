f4d81d7fe967ba84b612da14e287480d1ed82c56
Stefan Fodor <xtephan@gmail.com>
2011-09-20 11:52:06 +0200

    logserver is parsing the packages

--- src/server/logserver.py
+++ src/server/logserver.py
@@ -11,16 +11,18 @@ class LogServer():
     '''
 
 
-    def __init__(self):
-        '''
-        Constructor
-        '''
-    
-    def parsePackage(self):
+    def parsePackage(self,package):
         '''
         Returns a list containing the sensor number and value read
         '''
-        return 5
+        
+        ret    = package.split( '#' )
+        
+        ret[0] = int( ret[0]   )
+        ret[1] = int( ret[1],2 ) #convert from binary to decimal
+        
+        return ret
+    
     
     def sanity(self):
         return 4
--- src/unittests/testLogServer.py
+++ src/unittests/testLogServer.py
@@ -23,6 +23,15 @@ class Test(unittest.TestCase):
         pass
 
 
+    def test_parsePackage(self):
+        self.assertEqual(self.LS.parsePackage("1#101"), [1,5], 'parse 1 failed')
+        self.assertEqual(self.LS.parsePackage("2#001"), [2,1], 'parse 2 failed')
+        self.assertEqual(self.LS.parsePackage("3#111"), [3,7], 'parse 3 failed')
+        self.assertEqual(self.LS.parsePackage("4#000"), [4,0], 'parse 4 failed')
+        pass
+
+
+
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.test_sanity']
     unittest.main()
